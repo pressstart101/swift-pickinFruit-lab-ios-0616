4a4a10be99a3d4e8c3bad7e4bc0ceb413af65ca3
pressstart101 <walterscott404@yandex.com>
2016-07-31 13:46:52 -0400

    gambling is fun

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,25 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var pickerViewLabel: UIPickerView!
 
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
+    var numberOfRows = 1000
+    var numberOfComponents = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
+        self.pickerViewLabel.accessibilityLabel = Constants.FRUIT_PICKER
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        self.pickerViewLabel.delegate = self
+        self.pickerViewLabel.dataSource = self
         
+        for i in 0..<self.numberOfComponents {
+        let randomNumber = arc4random_uniform(UInt32(numberOfRows))
+        self.pickerViewLabel.selectRow(Int(randomNumber), inComponent: i, animated: true)
+        }
     }
     
     @IBAction func spinButtonTapped(sender: UIButton) {
-        
+        self.chooseFruit()
         
     }
+    
+    
+    func chooseFruit(){
+        for i in 0..<numberOfComponents{
+            let randomNumber = arc4random_uniform(UInt32(numberOfRows))
+        pickerViewLabel.selectRow(Int(randomNumber), inComponent: i, animated: true)
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return numberOfRows
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return numberOfComponents
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.fruitsArray[row % self.fruitsArray.count]
+    }
+    
     
 }
 
